676c1dda8654dca9a1505baa1788ba2ed4e82967
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-10-26 19:30:11 +0100

    feat: update og image

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://starlight-head-generator.vercel.app/',
+  site:
+    process.env['VERCEL_ENV'] !== 'production' && process.env['VERCEL_URL']
+      ? `https://${process.env['VERCEL_URL']}`
+      : 'https://starlight-head-generator.vercel.app/',
 })
